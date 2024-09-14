c1576ecef718f6d47427c7ed98bbd5ebc5a19882
lokesh768 <kadavakallulokesh@gmail.com>
2024-09-14 21:15:14 +0530

    gave allowed hosts to vercel

--- TrackMyEarnings/settings.py
+++ TrackMyEarnings/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-*dqo6wqs#7fzvn*#0(ctvqhgxplw+1e^8whsldc6$x)ho6h_d2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
