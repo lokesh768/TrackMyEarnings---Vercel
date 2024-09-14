05221691607eaa1feea5477c70f48683f9c3c77e
lokesh768 <kadavakallulokesh@gmail.com>
2024-09-14 19:12:44 +0530

    changed index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,6 @@
 from django.core.wsgi import get_wsgi_application
-import django
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TrackMyEarnings.settings")
-django.setup()
-application = get_wsgi_application()
+
+app = get_wsgi_application()
